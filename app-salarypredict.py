6de3170afc29f9e25c8f5f02e950dcff3867d381
Priya32pro <99943095+Priya-321@users.noreply.github.com>
2022-05-17 01:50:00 +0530

    Update app-salarypredict.py

--- app-salarypredict.py
+++ app-salarypredict.py
@@ -2,5 +2,5 @@ import streamlit as st
 from predict import show_predict
 
 
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
-show_predict_page()
+page = st.sidebar.selectbox("Predict", ("Predict Fresher's Salary"))
+show_predict()
